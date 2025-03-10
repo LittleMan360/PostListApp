88feb0e5ec529ebfcad5d06acd5f3af7417adbf7
Dalton Towe <dalton.m.towe@gmail.com>
2025-03-10 13:04:12 -0400

    Created posts visual and posts to update need to figure out binding

--- PostListApp/MainPage.xaml.cs
+++ PostListApp/MainPage.xaml.cs
@@ -1,25 +1,71 @@
-﻿namespace PostListApp
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
+
+namespace PostListApp
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        private const string ApiUrl = "https://jsonplaceholder.typicode.com/posts";
 
         public MainPage()
         {
             InitializeComponent();
+            _ = FetchPostsAsync();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async Task FetchPostsAsync()
         {
-            count++;
+            try
+            {
+                // Show loading indicator
+                LoadingIndicator.IsVisible = true;
+                LoadingIndicator.IsRunning = true;
+                ErrorMessageLabel.IsVisible = false;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                using HttpClient client = new HttpClient();
+                HttpResponseMessage response = await client.GetAsync(ApiUrl);
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    var posts = JsonConvert.DeserializeObject<List<Post>>(jsonResponse);
+
+                    // Bind data to ListView
+                    PostsListView.ItemsSource = posts;
+                }
+                else
+                {
+                    ShowErrorMessage("Failed to retrieve posts.");
+                }
+            }
+            catch (Exception ex)
+            {
+                ShowErrorMessage($"An error occurred while loading posts: {ex.Message}");
+            }
+            finally
+            {
+                // Hide loading indicator
+                LoadingIndicator.IsVisible = false;
+                LoadingIndicator.IsRunning = false;
+            }
+        }
+
+        private void ShowErrorMessage(string message)
+        {
+            ErrorMessageLabel.Text = message;
+            ErrorMessageLabel.IsVisible = true;
         }
     }
 
+    public class Post
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; } = string.Empty;
+        public string body { get; set; } = string.Empty;
+    }
 }
